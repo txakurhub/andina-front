9adcbadc45d1a1f471c60756781e8c2ee1df5812
Leandro Pereyra <me.leandrop@gmail.com>
2023-01-03 14:18:06 -0300

    addProgram

--- src/components/redux/actions/programActions.js
+++ src/components/redux/actions/programActions.js
@@ -21,6 +21,30 @@ export const getAllPrograms = () => {
     try {
       const json = await axios(`${URL_BASE}/programs`);
       dispatch({ type: GET_ALL_PROGRAMS_SUCCESS, payload: json.data });
-    } catch (error) {}
+    } catch (error) {
+      dispatch({ type: GET_ALL_PROGRAMS_REJECTED, payload: error });
+    }
+  };
+};
+
+export const addProgram = (payload, user) => {
+  return async function (dispatch) {
+    dispatch({ type: ADD_PROGRAM_PENDING });
+    try {
+      const json = await axios.post(
+        `${URL_BASE}/programs`,
+        payload,
+        { total: 1 },
+        {
+          headers: {
+            authorization: `jwt ${user}`,
+            "content-type": "application/json",
+          },
+        }
+      );
+      dispatch({ type: ADD_PROGRAM_SUCCESS, payload: json.data });
+    } catch (error) {
+      dispatch({ type: ADD_PROGRAM_REJECTED, payload: error });
+    }
   };
 };

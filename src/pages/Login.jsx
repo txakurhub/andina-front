574af710801ef83f6cc9a7522803eda1beb8482f
Leandro Pereyra <me.leandrop@gmail.com>
2023-01-06 12:12:18 -0300

    handlers login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,35 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
+import { userLogin } from "../components/redux/actions/loginActions";
 
 const Login = () => {
   const dispatch = useDispatch();
+  const [user, setUser] = useState({ user: "", password: "" });
 
+  const handleChange = (e) => {
+    e.preventDefault();
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    dispatch()
-  }
-    return (
+    e.preventDefault(userLogin());
+    dispatch();
+  };
+  return (
     <div>
       LOGIN
-      <form>
+      <form onSubmit={handleSubmit}>
         <h4>Usuario</h4>
-        <input type="text" placeholder="Usuario" />
+        <input type="text" placeholder="Usuario" onChange={handleChange} />
         <h4>Contraseña</h4>
-        <input type="password" placeholder="Contraseña" />
+        <input
+          type="password"
+          placeholder="Contraseña"
+          onChange={handleChange}
+        />
         <button type="submit">Ingresar</button>
       </form>
     </div>

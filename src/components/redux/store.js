92369841676ee249a1c5a1bed154c9b25dfc7311
Leandro Pereyra <me.leandrop@gmail.com>
2023-01-05 21:42:42 -0300

    actualizacion store

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
+import { loginReducer } from "./reducers/loginReducer";
 import { programReducer } from "./reducers/programReducer";
 
 const rootReducer = combineReducers({
   program: programReducer,
+  login: loginReducer,
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));

a7a9ee7279721a153b57b47dc7f6f6c29dcd4451
Leandro Pereyra <me.leandrop@gmail.com>
2023-01-10 12:26:22 -0300

    logo, react-router-dom, app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import { Provider } from "react-redux";
 import { store } from "./components/redux/store";
+import { BrowserRouter as Router, useRoutes } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
-function App() {
+const App = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/dashboard", element: <Login /> },
+  ]);
+  return routes;
+};
+
+const AppWrapper = () => {
   return (
     <Provider store={store}>
-      <div className="App">
-        <Home />
-      </div>
+      <Router>
+        <App />
+      </Router>
     </Provider>
   );
-}
+};
 
-export default App;
+export default AppWrapper;

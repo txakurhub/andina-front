ea4d87f908f48136dc6a7cb99eff03d9f376f581
Leandro Pereyra <me.leandrop@gmail.com>
2023-01-12 12:50:10 -0300

    folder fix

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { Grilla } from "../components/Grilla";
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -10,7 +11,11 @@ const Dashboard = () => {
     alert("No tienes permisos suficientes");
     navigate("/");
   } else {
-    return <div>DASHBOARD</div>;
+    return (
+      <div>
+        <Grilla />
+      </div>
+    );
   }
 };
 

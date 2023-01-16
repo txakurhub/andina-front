8c3ed4e8b8047993aa98f6213ebdc3f513f5241b
Leandro Pereyra <me.leandrop@gmail.com>
2023-01-16 08:58:39 -0300

    [app] editForm path

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import { BrowserRouter as Router, useRoutes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import EditForm from "./pages/EditForm";
 
 const App = () => {
   let routes = useRoutes([
     { path: "/", element: <Home /> },
     { path: "/login", element: <Login /> },
     { path: "/dashboard", element: <Dashboard /> },
+    { path: "/dashboard/edit", element: <EditForm /> },
+
   ]);
   return routes;
 };

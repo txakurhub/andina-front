e7d9830f8932638e0d62240c8769b581635207af
Leandro Pereyra <me.leandrop@gmail.com>
2023-01-23 08:48:03 -0300

    sidebar format II

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,16 @@
 const Sidebar = () => {
   return (
     <div>
-      {/* //programas
-            //agregar
-            //editar
-            //eliminar
-            //cerrar sesión */}
+      <div>
+        <h4>Programas</h4>
+        <div>
+          <p>Ver</p>
+          <p>Agregar</p>
+          <p>Editar</p>
+          <p>Eliminar</p>
+        </div>
+      </div>
+      <h4>Cerrar sesión</h4>
     </div>
   );
 };
